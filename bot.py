b5f10cf24d1ed2463d385618dd1d05fd2a7d191f
Flokie <61638280+FlokieW@users.noreply.github.com>
2024-06-06 11:59:31 +0200

    another one

--- bot.py
+++ bot.py
@@ -75,7 +75,8 @@ async def ranks(ctx):
 
                 # Check each user's roles
                 for index, row in df.iterrows():
-                    member = discord.utils.get(bot.get_all_members(), name=row['Discord Username'])
+                    member = discord.utils.get(ctx.guild.members, name=row['Discord Username'])
+
                     if member:
                         for role in member.roles:
                             if role.name in CONFIGURED_ROLES:
